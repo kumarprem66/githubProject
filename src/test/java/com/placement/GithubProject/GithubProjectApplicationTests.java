1773ba48d86a8fbe3269e0c4e0491f942639228e
kumarprem66 <premstart10@gmail.com>
2023-10-05 12:17:13 +0530

    checking

---
+++ src/test/java/com/placement/GithubProject/GithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placement.GithubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
